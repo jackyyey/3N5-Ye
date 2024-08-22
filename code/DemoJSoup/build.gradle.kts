93d174ff012a3d118f232af380573c201857e160
Jacky Ye <6272880@cegepmontpetit.ca>
2024-08-22 16:25:04 -0400

    INIT: création exercice DemoJSoup

---
+++ code/DemoJSoup/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "org.ye"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("org.jsoup:jsoup:1.18.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
