6522a81c43b37b1d9162393352a41ebba2ce6cba
Jacky Ye <6272880@cegepmontpetit.ca>
2024-08-22 09:36:22 -0400

    Ajout exercice Triangle

---
+++ code/BonjoutKotlin/src/main/kotlin/Triangle.kt
@@ -0,0 +1,11 @@
+package org.ye
+
+fun main(){
+    val nb = 4
+        for (i in 1..nb){
+            for (j in 1 .. i){
+                print("*")
+            }
+            println()
+        }
+}
