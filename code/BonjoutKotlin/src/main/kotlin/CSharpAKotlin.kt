73016bd5aa2db27fc89ff15313b708faf4b60620
Jacky Ye <6272880@cegepmontpetit.ca>
2024-08-22 09:29:48 -0400

    Ajout exercice C#AKotlin

---
+++ code/BonjoutKotlin/src/main/kotlin/CSharpAKotlin.kt
@@ -0,0 +1,14 @@
+package org.ye
+
+fun main(args: Array<String>) {
+    if (args.isNotEmpty()) {
+        val nom = args[0]
+        afficher(nom)
+    } else {
+        println("SVP veuillez fournir un nom!")
+    }
+}
+
+fun afficher(nom: String) {
+    println("Bonjour $nom!")
+}
