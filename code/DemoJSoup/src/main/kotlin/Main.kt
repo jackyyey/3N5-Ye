481904c3559214e22357eebe1913e018e4550e15
Jacky Ye <6272880@cegepmontpetit.ca>
2024-08-25 04:20:52 -0400

    FCT: Exemple JSoup
    Ajout du code source d'un exemple tiré du site web jsoup.org

--- code/DemoJSoup/src/main/kotlin/Main.kt
+++ code/DemoJSoup/src/main/kotlin/Main.kt
@@ -1,16 +1,20 @@
 package org.ye
 
+import org.jsoup.Jsoup
+
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val doc: Document = Jsoup.connect("https://en.wikipedia.org/").get()
+    log(doc.title())
+    val newsHeadlines: Elements = doc.select("#mp-itn b a")
+    for (headline in newsHeadlines) {
+        log(
+            "%s\n\t%s",
+            headline.attr("title"), headline.absUrl("href")
+        )
     }
+
 }
