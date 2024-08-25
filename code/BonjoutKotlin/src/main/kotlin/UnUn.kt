eb2c8c1dc02fe66fe8ed9e501f06caec4715b891
Jacky Ye <6272880@cegepmontpetit.ca>
2024-08-25 14:29:07 -0400

    PROGRES: Exercice UnUn

--- code/BonjoutKotlin/src/main/kotlin/UnUn.kt
+++ code/BonjoutKotlin/src/main/kotlin/UnUn.kt
@@ -1,24 +1,27 @@
 package org.ye
 fun main() {
-    unUn("111213")
+    println(unUn("111213"))
 }
 fun unUn(source: String): String{
-    var cnt: Int = 0
+    var cnt: Int = 1
     var num: Int = 0
     var newstring = ""
-        for(i:Int in 0..source.length-1){
-            num = source[i].digitToInt()
-            var nextIndex = i;
-            if (i<source.length-1 && source[i] == source[nextIndex+1]\)
+        for(i:Int in 0 .. source.length-1){
+            if (i!=0)
             {
-                cnt++
-            }
-            else
-            {
-                newstring+="$cnt$num"
+                if (source[i].digitToInt() == source[i-1].digitToInt()){
+                    cnt++
+                }
+                else
+                {
+                    newstring+="$cnt$num"
+                    cnt = 1
+                }
+                num=source[i].digitToInt()
             }
 
-    }
+        }
     return newstring
 }
+
 
