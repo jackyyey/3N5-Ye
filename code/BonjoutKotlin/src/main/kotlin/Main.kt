d32ea3ea127fa0d74c89a50ff8128aae55b49da7
Jacky Ye <6272880@cegepmontpetit.ca>
2024-08-27 11:30:13 -0400

    Ajout TraceManuelle.md

--- code/BonjoutKotlin/src/main/kotlin/Main.kt
+++ code/BonjoutKotlin/src/main/kotlin/Main.kt
@@ -1,16 +1,13 @@
 package org.ye
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    for (i : Int in 1..1) {
+        for (j : Int in 1..2) {
+            for (k : String in listOf(" pif", "") ) {
+                val b = .1 + .1 + .1
+                val z = (i + j * b / k.length * j).toString() + k
+                println(z + " " + b)
+            }
+        }
     }
 }
---
+++ code/BonjoutKotlin/src/main/kotlin/TraceManuelle.md
@@ -0,0 +1,5 @@
+1.15 pif 0.3
+1.6  0.3
+
+b/k.length = 0.75
+b/k.length = 0
