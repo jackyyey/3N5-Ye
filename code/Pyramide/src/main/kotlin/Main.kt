dc112914221be7fafcb61f9cf1578bb747098dd1
Jacky Ye <6272880@cegepmontpetit.ca>
2024-08-29 09:11:57 -0400

    FCT: Exercice Pyramide
    Je l'ai rendu fonctionnel

--- code/Pyramide/src/main/kotlin/Main.kt
+++ code/Pyramide/src/main/kotlin/Main.kt
@@ -2,10 +2,30 @@ package org.ye
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-
+fun main(args: Array<String>) {
+    for(nbPyramide in args)
+    {
+        print(Pyramide(nbPyramide.toInt()))
+    }
 }
 
 fun Pyramide(hauteur:Int): String{
-    return "TODO"
+    var Pyramide = ""
+    var nbEtoile = 1
+    for (col in 1..hauteur)
+    {
+        var espace = hauteur-col
+        for (i in 1..espace)
+        {
+            Pyramide+=" "
+        }
+        for(j in 1..nbEtoile)
+        {
+            Pyramide+="*"
+            Pyramide+" "
+        }
+        nbEtoile+=2
+        Pyramide+='\n'
+    }
+    return Pyramide
 }
