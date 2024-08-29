20b05d3e745fd909b73a2f73088b7c3a507a16fb
Jacky Ye <6272880@cegepmontpetit.ca>
2024-08-29 08:39:28 -0400

    INIT exercice Pyramide

---
+++ code/Pyramide/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+package org.ye
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+}
+
+fun Pyramide(hauteur:Int): String{
+    return "TODO"
+}
