6276e85baea38f0b65d27236461729f3d42671fd
Jacky Ye <6272880@cegepmontpetit.ca>
2024-08-29 08:31:29 -0400

    INIT: Exercice Types

---
+++ code/Types/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+package org.ye
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val a = 1                                   // type est
+    val aa = 1L                                 // type est
+    val aaa = a * 25 + aa * 100                 // type est
+    val b = 1.0                                 // type est
+    val c = "1"                                 // type est
+    val d = true                                // type est
+    val laChose = c + b                         // type est
+    val k = listOf("1", "2", "3")               // type est
+    val l = mutableListOf(true, false, true)    // type est
+}
