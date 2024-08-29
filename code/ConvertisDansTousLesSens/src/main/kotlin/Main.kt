11ae3549b50c07b1426fa43333f76d2d5eb19866
Jacky Ye <6272880@cegepmontpetit.ca>
2024-08-29 09:52:43 -0400

    INIT: Exercice ConvertisDansTousLesSens

---
+++ code/ConvertisDansTousLesSens/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+package org.ye
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var int = 2
+    var float = int.toFloat()
+    var double: Double = 2.4232
+    var int2 = double.toInt()
+    var string: String = "1244676543"
+    var int3 = string.toInt()
+    var double2: Double = float.toDouble()
+}
