2d4d2c7f9a135532279f7ae2eedf9f25b706b464
Jacky Ye <6272880@cegepmontpetit.ca>
2024-08-29 09:29:00 -0400

    INIT: Exercice Moyenne

---
+++ code/ExerciceMoyenne/src/main/kotlin/Main.kt
@@ -0,0 +1,64 @@
+package org.ye
+
+import kotlin.random.Random
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var tabAleatoire = creeTableauAleatoire(10)
+    println(Somme(tabAleatoire))
+    println(Moyenne(tabAleatoire))
+    println(Max(tabAleatoire))
+    println(Min(tabAleatoire))
+}
+fun creeTableauAleatoire(n: Int): Array<Int> {
+    var tabIntAleatoire = emptyArray<Int>()
+    for (i in 0..n)
+    {
+        tabIntAleatoire += Random.nextInt(0,100)
+    }
+    return tabIntAleatoire
+}
+
+fun Somme(n: Array<Int>): Int{
+    var nb = 0
+    for (i in 0..n.size)
+    {
+        nb+=n[i]
+    }
+    return nb
+}
+
+fun Moyenne(n: Array<Int>): Int{
+    var nb = 0
+    for (i in 0..n.size)
+    {
+        nb+=n[i]
+    }
+    return nb/n.size
+}
+
+fun Max(n: Array<Int>): Int{
+    var nb = 0
+    for (i in 0..n.size)
+    {
+        if (n[i]>=nb)
+        {
+            nb = n[i]
+        }
+    }
+    return nb
+}
+
+fun Min(n: Array<Int>): Int{
+    var nb = 0
+    for (i in 0..n.size)
+    {
+        if (n[i]<=nb)
+        {
+            nb = n[i]
+        }
+    }
+    return nb
+}
+
