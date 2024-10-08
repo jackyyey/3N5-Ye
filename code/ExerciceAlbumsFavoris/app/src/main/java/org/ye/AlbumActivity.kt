114e6e1af56ebd9ee87f34461a68f43ac5e4345c
Jacky Ye <6272880@cegepmontpetit.ca>
2024-10-08 12:56:48 -0400

    PROGRES: Exercice Albums Favoris: Details
    mon xml marche!!!!

---
+++ code/ExerciceAlbumsFavoris/app/src/main/java/org/ye/AlbumActivity.kt
@@ -0,0 +1,17 @@
+package org.ye
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.ye.databinding.ActivityAlbumBinding
+
+
+class AlbumActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAlbumBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
---
+++ code/ExerciceAlbumsFavorisDetail/app/src/main/java/org/ye/MainActivity.kt
@@ -0,0 +1,38 @@
+package org.ye
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import org.ye.adapters.AlbumAdapter
+import org.ye.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: AlbumAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        setupRecycler()
+        fillRecycler()
+    }
+    private fun setupRecycler(){
+        adapter = AlbumAdapter()
+        binding.rvAlbumAdapter.adapter = adapter
+        binding.rvAlbumAdapter.setHasFixedSize(true)
+        binding.rvAlbumAdapter.addItemDecoration(
+            DividerItemDecoration(binding.rvAlbumAdapter.context, DividerItemDecoration.VERTICAL)
+        )
+    }
+    private fun fillRecycler() {
+        val items: MutableList<String> = mutableListOf()
+        for (i in 1..10) {
+            items.add("Item #$i")
+        }
+        adapter.submitList(items) // Pour changer le contenu de la liste, utiliser submitList de l'adapteur
+    }
+
+}
