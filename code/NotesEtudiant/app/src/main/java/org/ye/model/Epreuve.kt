5a903e836766f5e9af527806441843cbb00b68a2
Jacky Ye <6272880@cegepmontpetit.ca>
2024-11-12 11:29:20 -0500

    INIT ExerciceNotes

---
+++ code/NotesEtudiant/app/src/main/java/org/ye/model/Epreuve.kt
@@ -0,0 +1,9 @@
+package org.ye.model
+
+data class Epreuve(
+    var note: Int,
+    var etudiant: Etudiant,
+    var ponderation: Int
+) {
+
+}
---
+++ code/NotesEtudiant/app/src/main/java/org/ye/model/Etudiant.kt
@@ -0,0 +1,9 @@
+package org.ye.model
+
+data class Etudiant(
+    var nom : String,
+    var epreuvres : List<Epreuve>
+){
+
+
+}
