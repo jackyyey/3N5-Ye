2c882ac98f813d9749089329baed2b5e34330661
Jacky Ye <6272880@cegepmontpetit.ca>
2025-01-28 09:52:15 -0500

    fct Exercice MiseEnPageD

---
+++ code-4n5/MiseEnPageD/app/src/main/java/org/ye/MainActivity.kt
@@ -0,0 +1,19 @@
+package org.ye
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.ye.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        var view = binding.root
+        setContentView(view)
+
+    }
+}
