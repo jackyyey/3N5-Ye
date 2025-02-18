05761fa6f285d6f40c2cef6d735842eb0fa4e61d
Jacky Ye <6272880@cegepmontpetit.ca>
2025-02-18 11:02:12 -0500

    PROGRES: Retrofitliste

--- code-4n5/RetrofitListe/app/src/main/java/org/ye/MainActivity.kt
+++ code-4n5/RetrofitListe/app/src/main/java/org/ye/MainActivity.kt
@@ -8,7 +8,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.DividerItemDecoration
 import org.depinfo.retrofit_demo.http.RetrofitUtil
+import org.ye.adapters.LongAdapter
+import org.ye.adapters.PersonneAdapter
 import org.ye.databinding.ActivityMainBinding
 import org.ye.service.Service
 import retrofit2.Call
@@ -17,24 +20,36 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: PersonneAdapter
+    private lateinit var adapterLong: LongAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnDouble.setOnClickListener({
-            val service: Service = RetrofitUtil.get()
-            val nom: String = binding.txPersonne.text.toString()
-            val call: Call<Personne> = service.getPersonne(nom)
-            call.enqueue(object: Callback<Personne>{
-                override fun onResponse(call: Call<Personne>,response: Response<Personne>){
-                    if (response.isSuccessful){
-                        binding.txNom.text = response.body()!!.b
-                    }
+        val service: Service = RetrofitUtil.get()
+        val callLong: Call<List<Long>> = service.getListLong()
+        val callComplex: Call<List<Personne>> = service.getListComplex()
+        call.enqueue(object: Callback<Personne>{
+            override fun onResponse(call: Call<Personne>,response: Response<Personne>){
+                if (response.isSuccessful){
+                    binding.txNom.text = response.body()!!.b
                 }
-                override fun onFailure(call: Call<Personne>, t: Throwable) {
-                    t.printStackTrace()
-                }
-            })
+            }
+            override fun onFailure(call: Call<Personne>, t: Throwable) {
+                t.printStackTrace()
+            }
         })
     }
+    private fun setupRecycler() {
+        adapter = PersonneAdapter() // Créer l'adapteur
+        adapterLong = LongAdapter() // Créer l'adapteur long
+        binding.rvRecyclerComplex.adapter = adapter // Assigner l'adapteur au RecyclerView
+        binding.rvRecyclerComplex.setHasFixedSize(true) // Option pour améliorer les performances
+        binding.rvRecyclerComplex.addItemDecoration( // Ajouter un séparateur entre chaque élément
+            DividerItemDecoration(
+                binding.rvRecyclerComplex.context, DividerItemDecoration.VERTICAL
+            )
+        )
+    }
+
 }
--- code-4n5/RetrofitListe/app/src/main/java/org/ye/service/Service.kt
+++ code-4n5/RetrofitListe/app/src/main/java/org/ye/service/Service.kt
@@ -7,7 +7,10 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface Service {
-    @GET("/exos/truc/complexe")
-    fun getPersonne(@Query("name") name: String): Call<Personne>
+    @GET("/exos/long/list")
+    fun getListLong(): Call<List<Long>>
+
+    @GET("/exos/truc/list")
+    fun getListComplex(): Call<List<Personne>>
 }
 
