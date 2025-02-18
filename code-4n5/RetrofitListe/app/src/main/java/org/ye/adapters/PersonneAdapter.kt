fe8a7056d7465494d055d5f354f9ca330d85db55
Jacky Ye <6272880@cegepmontpetit.ca>
2025-02-18 10:40:05 -0500

    PROGRES: RetrofitListe

---
+++ code-4n5/RetrofitListe/app/src/main/java/org/ye/adapters/PersonneAdapter.kt
@@ -0,0 +1,43 @@
+package org.ye.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import org.ye.Personne
+import org.ye.databinding.ObjetcomplexItemBinding
+
+class PersonneAdapter : ListAdapter<Personne, PersonneAdapter.PersonneItemViewHolder>(PersonneItemDiffCallback) {
+
+    // binding nous permet d'accéder à tout le champs de notre layout personne_item.xml
+    inner class PersonneItemViewHolder(private val binding: ObjetcomplexItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(personne: Personne) {
+            binding.txChiffre.text = personne.a.toString()
+            binding.txChiffre.text = personne.b
+            binding.txChiffre.text = personne.c?.size.toString()
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonneItemViewHolder {
+        val binding: ObjetcomplexItemBinding = ObjetcomplexItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PersonneItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PersonneItemViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+object PersonneItemDiffCallback : DiffUtil.ItemCallback<Personne>() {
+    override fun areItemsTheSame(oldItem: Personne, newItem: Personne): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Personne, newItem: Personne): Boolean {
+        return oldItem.a == newItem.a &&
+                oldItem.b == newItem.b &&
+                oldItem.c == newItem.c
+    }
+}
