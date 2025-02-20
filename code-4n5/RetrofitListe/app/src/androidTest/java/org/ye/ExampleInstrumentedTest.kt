48d3781af272622574e21b68cfc1eb85061a8a5c
Jacky Ye <6272880@cegepmontpetit.ca>
2025-02-20 13:14:39 -0500

    FCT: Exercice RetrofitEnvoiPost

--- code-4n5/RetrofitListe/app/src/androidTest/java/org/ye/ExampleInstrumentedTest.kt
+++ code-4n5/RetrofitListe/app/src/androidTest/java/org/ye/ExampleInstrumentedTest.kt
@@ -2,11 +2,14 @@ package org.ye
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.depinfo.retrofit_demo.http.RetrofitUtil
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.ye.service.Service
+import retrofit2.Call
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +23,18 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("org.ye", appContext.packageName)
+    }
+    @Test
+    fun envoieposte(){
+        val service: Service = RetrofitUtil.get()
+        val request = Personne()
+        request.a = 100
+        request.b = "gndsadsa"
+        request.c = listOf<Long>(8,12,14,18,1998).toTypedArray()
+        val call: Call<Personne> = service.postObjet(request)
+        val response = call.execute()
+        val resultat = response.body()
+        println("Retour du signup : " + resultat)
+        assert(resultat != null)
     }
 }
--- code-4n5/RetrofitListe/app/src/main/java/org/ye/service/Service.kt
+++ code-4n5/RetrofitListe/app/src/main/java/org/ye/service/Service.kt
@@ -1,8 +1,11 @@
 package org.ye.service
 
+import android.app.Person
 import org.ye.Personne
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -12,5 +15,8 @@ interface Service {
 
     @GET("/exos/truc/list")
     fun getListComplex(): Call<List<Personne>>
+
+    @POST("exos/truc/doubler")
+    fun postObjet(@Body personne: Personne): Call<Personne>
 }
 
